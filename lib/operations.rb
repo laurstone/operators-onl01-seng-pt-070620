0e01f56a8afdb80e675a70b640a4c44d2f395688
Lauren Stone <lstone321@gmail.com>
2020-07-10 02:19:15 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -9,11 +9,5 @@ end
 
 def not_safe?(speed)
 	speed<40 || speed>60 ? true:false
-	return "safe"
-end
-	
-
-
-def not_safe?(speed)
-  speed < 40 || speed > 60 ? true : false
+	return "safe" unless "false"
 end
